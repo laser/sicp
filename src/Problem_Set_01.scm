9308f99dc32b543624b95e879eadc9da7e3ff420
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-09-27 16:30:52 -0700

    problem set 1 implementation

--- src/Problem_Set_01.scm
+++ src/Problem_Set_01.scm
@@ -17,8 +17,10 @@
 (define fold
   (lambda (x y)
     (* (spindle x)
-       (+ (mutilate y)
-	  (spindle x y)))))
+       (+ (mutilate y) (spindle x))
+    )
+  )
+)
 
 (define spindle
   (lambda (w) (* w w)))
@@ -29,6 +31,42 @@
 
 (define fact
   (lambda (n)
-    (if (= n 0)		
-	m
-	(* n (fact (- n 1))))))
+    (if (= n 0) 1 (* n (fact (- n 1))))))
+
+(define comb
+  (lambda (n k)
+    (/ (fact n) (* (fact k) (fact (- n k))))))
+
+(define raise
+  (lambda (n pow)
+    (cond ((= pow 0) 1)
+          ((= pow 1) n)
+          (else (* n (raise n (- pow 1)))))))
+
+(define square
+  (lambda (n)
+    (raise n 2)))
+
+(define cube
+  (lambda (n)
+    (raise n 3)))
+
+(define sum-of-cubes
+  (lambda (x y)
+    (+ (cube x) (cube y))))
+
+(define biggest
+  (lambda (x y)
+    (if (> x y) x y)))
+
+(define biggest-of-three
+  (lambda (x y z)
+    (biggest x (biggest y z))))
+
+(define cube-biggest-of-three
+  (lambda (x y z)
+    (cube (biggest-of-three x y z))))
+
+(define f
+  (lambda (x a0 a1 a2 a3)
+    (+ a0 (* a1 x) (* a2 (square x)) (* a3 (cube x)))))
