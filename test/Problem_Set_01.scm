908c2e3b1aae5d44d2e604a7878d4ef3ff38fcfe
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-09-27 16:32:16 -0700

    test suite for problem set 1

--- test/Problem_Set_01.scm
+++ test/Problem_Set_01.scm
@@ -2,4 +2,12 @@
 (in-test-group
   Problem_Set_01
   (define-each-test
-    (assert-= 4 (+ 2 2) "Two and two should make four.")))
+    (assert-= 120 (fact 5) "ex2: fact computes the factorial of a positive number")
+    (assert-= 3 (comb 3 2) "ex3: combinations of n things taken k at a time")
+    (assert-= 27 (cube 3) "ex10: return the cube of a value")
+    (assert-= 54 (sum-of-cubes 3 3) "ex11: return the sum of cubing each argument")
+    (assert-= 5 (biggest-of-three 2 5 1) "ex12: return the 'biggest' of three values")
+    (assert-= 125 (cube-biggest-of-three 2 5 1) "ex13: return the cube of the 'biggest' of three")
+    (assert-= 3.25 (f 0.5 1 2 3 4) "ex14: evaluating 3rd degree polynomials")
+  )
+)
