68fcd1d65836bcc3b157d8cbd95b10aaf87ac3b2
Neil Menne <nhm@neilmenne.com>
2014-10-11 20:14:44 -0400

    example 1.18: fast multiply

--- src/Optional_Assignments_02.scm
+++ src/Optional_Assignments_02.scm
@@ -8,3 +8,15 @@
 ;; f(n) = (A 0 n) => f(n) = 2*n
 ;; g(n) = (A 1 n) => g(n) = 2^n
 ;; h(n) = (A 2 n) => h(n) = 2^(2^...n times) i.e h(4) = 2^(2^(2^2)) = 2^(2^4) = 2^16 = 65536
+
+;; Exercise 1.18
+(define (fast-multiply a b)
+  (define (double x) (+ x x))
+  (define (halve x) (/ x 2))
+  (define (multiply-iter a b product)
+    (cond
+     ((= b 0) product)
+     ((even? b) (multiply-iter (double a) (halve b) product))
+     (else (multiply-iter a (- b 1) (+ a product)))))
+  (multiply-iter a b 0)
+)
--- test/Optional_Assignments_02.scm
+++ test/Optional_Assignments_02.scm
@@ -1,9 +1,12 @@
 (load "src/Optional_Assignments_02.scm")
 (in-test-group
   Optional_Assignments_02
-  (define-each-test
-    (assert-= 1024 (A 1 10) "Exercise 1.10: Solve the following: 2^10")
-    (assert-= 65536 (A 2 4) "Exercise 1.10: Solve the following: 2^16")
-    (assert-= 65536 (A 3 3) "Exercise 1.10: Solve the following: 2^16")
+  (define-test (ackermans-number)
+    (assert-equal 1024 (A 1 10) "Exercise 1.10: Solve the following: 2^10")
+    (assert-equal 65536 (A 2 4) "Exercise 1.10: Solve the following: 2^16")
+    (assert-equal 65536 (A 3 3) "Exercise 1.10: Solve the following: 2^16")
+  )
+  (define-test (fast-multiply-test)
+    (assert-equal 10 (fast-multiply 5 2) "Fast multiply works just as good as multiply")
   )
 )
